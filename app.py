4d194d064c785bc6b4faa8ccdfcd9489ccdca0a7
Dimas Ardiminda <113449449+DimzsArdiminda@users.noreply.github.com>
2024-03-14 03:49:39 +0700

    "new bismillah ga error"

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 # Load dataset
 data = pd.read_csv('data.csv')
@@ -17,23 +18,28 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 knn_model = KNeighborsClassifier(n_neighbors=3)
 knn_model.fit(X_train, y_train)
 
+# Lakukan prediksi pada data test
+y_pred = knn_model.predict(X_test)
+
+# Hitung akurasi
+accuracy = accuracy_score(y_test, y_pred)
+
 # Inisialisasi aplikasi Flask
 app = Flask(__name__, template_folder='.')
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    prediction = None
-    if request.method == 'POST':
-        ip1 = float(request.form['ip1'])
-        ip2 = float(request.form['ip2'])
-        ip3 = float(request.form['ip3'])
-        ip4 = float(request.form['ip4'])
-        
-        # akurasi
-        prediction = knn_model.predict([[ip1, ip2, ip3, ip4]])
+    return render_template('index.html', accuracy=accuracy)
 
-        # Hitung akurasi model
-        accuracy = knn_model.score(X_test, y_test)
+@app.route('/', methods=['POST'])
+def predict():
+    ip1 = float(request.form['ip1'])
+    ip2 = float(request.form['ip2'])
+    ip3 = float(request.form['ip3'])
+    ip4 = float(request.form['ip4'])
+
+    # Lakukan prediksi
+    prediction = knn_model.predict([[ip1, ip2, ip3, ip4]])
 
     return render_template('index.html', prediction=prediction[0], accuracy=accuracy)
 
